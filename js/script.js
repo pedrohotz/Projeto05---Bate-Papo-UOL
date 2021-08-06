fe0258f4c1a56c95dd316376a78f5615160d87a0
Pedro Hotz <pedrohotz2002@gmail.com>
2021-08-06 12:54:46 -0300

    Enviar mensagem para TODOS adicionado(codições para cores de tipo de mensagem adicionado)

--- js/script.js
+++ js/script.js
@@ -1,4 +1,3 @@
-
 let usuario;
 let mensagens = [];
 function perguntarNome(){
@@ -12,7 +11,7 @@ function entrarSala(){
     const promisse = axios.post("https://mock-api.bootcamp.respondeai.com.br/api/v3/uol/participants",{name:usuario});
     promisse.catch(error);
     promisse.then(enviarStatus);
-    promisse.then(recarregarMsg);
+    promisse.then(buscarMensagens);
 }
 
 function error(resposta){
@@ -46,7 +45,30 @@ function listarMsg(resposta){
     mensagens = resposta.data;
     divMsg.innerHTML = "";
    for(let i = 0; i < mensagens.length;i++){
-        divMsg.innerHTML += ` <div class="msg"><div class="hora">${mensagens[i].time}</div> <div class="nome"><strong>${mensagens[i].from}</strong></div>${mensagens[i].text}</div>`
-   }
+    if(mensagens[i].type === "message"){
+        divMsg.innerHTML += ` <div class="msg"><div class="hora">${mensagens[i].time} </div> <div class="nome"><strong>${mensagens[i].from} </strong>para<strong> ${mensagens[i].to}: </strong></div>${mensagens[i].text}</div>`
+    }
+    else if(mensagens[i].type === "status"){
+        divMsg.innerHTML += ` <div class="msg entrar-sair"><div class="hora">${mensagens[i].time}</div> <div class="nome"><strong>${mensagens[i].from}</strong></div>${mensagens[i].text}</div>`
+    }
+    else if(mensagens[i].type === "private_message"){
+        divMsg.innerHTML += ` <div class="msg private"><div class="hora">${mensagens[i].time}</div> <div class="nome"><strong>${mensagens[i].from}</strong>reservadamente para ${mensagens[i].to}:</div>${mensagens[i].text}</div>`
+    }
+}
    divMsg.lastChild.scrollIntoView();
+   enviarStatus();
+   recarregarMsg();
+}
+
+
+function enviarMsg(){
+    const msg = document.querySelector(".msg-enviar").value
+    const mensagem = {
+        from: usuario,
+        to: "Todos",
+        text: msg,
+        type: "message",
+    }
+    const promisse = axios.post("https://mock-api.bootcamp.respondeai.com.br/api/v3/uol/messages",mensagem);
+    promisse.then(buscarMensagens);
 }
