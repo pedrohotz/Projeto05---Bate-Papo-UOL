49a4f469bf74f5927210894d279f25a71fb46577
Pedro Hotz <pedrohotz2002@gmail.com>
2021-08-06 11:29:34 -0300

    Chat com rolagem automatica para ultima mensagem

--- js/script.js
+++ js/script.js
@@ -1,18 +1,18 @@
 
-let usuario = "dawhjafahj";
+let usuario;
 let mensagens = [];
 function perguntarNome(){
  usuario = prompt("Digite o nome de usuario:")
 }
 
-//perguntarNome();
+perguntarNome();
 entrarSala();
 
 function entrarSala(){
     const promisse = axios.post("https://mock-api.bootcamp.respondeai.com.br/api/v3/uol/participants",{name:usuario});
     promisse.catch(error);
     promisse.then(enviarStatus);
-    promisse.then(buscarMensagens);
+    promisse.then(recarregarMsg);
 }
 
 function error(resposta){
@@ -29,6 +29,12 @@ function enviarStatus(){
 
 }
 
+function recarregarMsg(){
+    setInterval(function(){
+        buscarMensagens();
+    },3000);
+}
+
 function buscarMensagens(){
     const promisse = axios.get("https://mock-api.bootcamp.respondeai.com.br/api/v3/uol/messages")
     promisse.then(listarMsg);
@@ -39,8 +45,8 @@ function listarMsg(resposta){
     const divMsg = document.querySelector(".chat");
     mensagens = resposta.data;
     divMsg.innerHTML = "";
-    console.log(mensagens.length);
    for(let i = 0; i < mensagens.length;i++){
         divMsg.innerHTML += ` <div class="msg"><div class="hora">${mensagens[i].time}</div> <div class="nome"><strong>${mensagens[i].from}</strong></div>${mensagens[i].text}</div>`
    }
+   divMsg.lastChild.scrollIntoView();
 }
