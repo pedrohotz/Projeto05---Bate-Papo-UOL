871e2d4e94292c8f84fe71050220e3bedd604dbc
Pedro Hotz <pedrohotz2002@gmail.com>
2021-08-09 13:54:34 -0300

    Funcionalidade padrão implementadas(bonus de selecioanr destinatario) com erros de layout

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
 let usuario;
 let mensagens = [];
+let destinatario = "Todos";
+let type = "message";
 function perguntarNome(){
  usuario = prompt("Digite o nome de usuario:")
 }
@@ -9,15 +11,17 @@ entrarSala();
 
 function entrarSala(){
     const promisse = axios.post("https://mock-api.bootcamp.respondeai.com.br/api/v3/uol/participants",{name:usuario});
-    promisse.catch(error);
     promisse.then(enviarStatus);
     promisse.then(buscarMensagens);
     promisse.then(recarregarMsg);
+    promisse.catch(tratarError);
 }
 
-function error(resposta){
-    if(resposta.reponse.status==400){
-        perguntarNome();
+function tratarError(resposta){
+    status_code = resposta.response.status;
+    if(status_code === 400){
+        usuario = prompt("Nome de usuario em uso, digite outro:");
+        entrarSala();
     }
 }
 
@@ -53,8 +57,8 @@ function listarMsg(resposta){
     else if(mensagens[i].type === "status"){
         divMsg.innerHTML += ` <div class="msg entrar-sair"><div class="hora">${mensagens[i].time}</div> <div class="nome"><strong>${mensagens[i].from}</strong></div>${mensagens[i].text}</div>`
     }
-    else if(mensagens[i].type === "private_message" && mensagens[i].to === usuario){
-        divMsg.innerHTML += ` <div class="msg private"><div class="hora">${mensagens[i].time}</div> <div class="nome"><strong>${mensagens[i].from}</strong>reservadamente para ${mensagens[i].to}:</div>${mensagens[i].text}</div>`
+    else if(mensagens[i].type === "private_message" && mensagens[i].to === usuario || mensagens[i].from === usuario){
+        divMsg.innerHTML += ` <div class="msg private"><div class="hora">${mensagens[i].time}</div> <div class="nome"><strong>${mensagens[i].from} </strong>reservadamente para ${mensagens[i].to}:</div>${mensagens[i].text}</div>`
     }
 }
    divMsg.lastChild.scrollIntoView();
@@ -66,12 +70,13 @@ function enviarMsg(){
     const msg = document.querySelector(".msg-enviar").value
         const mensagem = {
             from: usuario,
-            to: "Todos",
+            to: destinatario,
             text: msg,
-            type: "message",
+            type: type,
         }
         const promisse = axios.post("https://mock-api.bootcamp.respondeai.com.br/api/v3/uol/messages",mensagem);
         promisse.then(buscarMensagens);
+        mensagens.push(mensagem);
         promisse.catch(errorServer);
 }
 
@@ -82,25 +87,52 @@ function errorServer(){
 
 function abrirLateral(){
     const menuLat = document.querySelector(".mascara");
-    const chat = document.querySelector(".chat");
     const promisse = axios.get("https://mock-api.bootcamp.respondeai.com.br/api/v3/uol/participants");
     promisse.then(listarParticipants);
     if (menuLat.classList.contains("hide")){
         menuLat.classList.remove("hide");
     }
-    else{
-         chat.addEventListener('onClick',function(){
-             menuLat.classList.add("hide");
-         })
-    }
 }
+
+
+function fecharMenu(){
+    const menuLat = document.querySelector(".mascara");
+    menuLat.classList.add("hide");
+
+}
+
 
 function listarParticipants(resposta){
      let participantes = resposta.data;
-     console.log(participantes);
      let listaPart = document.querySelector(".participants");
     for(let i = 0;i < participantes.length;i++){
-        listaPart.innerHTML += `<li><div class="nome-icone"><ion-icon name="people"></ion-icon><span>${participantes[i].name}</span></div><ion-icon name="checkmark" class="check hide"></ion-icon></li>`
+        listaPart.innerHTML += `<li onclick="selecionarDest(this);"><div class="nome-icone"><ion-icon name="people"></ion-icon><span>${participantes[i].name}</span></div><ion-icon name="checkmark" class="check"></ion-icon></li>`
     }
 
+}
+
+function selecionarDest(dest){
+    const selecionado = document.querySelector(".selecionado");
+    console.log(selecionado);
+    if (selecionado !== null){
+        selecionado.classList.remove("selecionado");
+    }
+    dest.classList.add("selecionado");
+    destinatario = dest.querySelector("span").innerHTML;
+}
+
+function selecionarReservado(){
+    const publico = document.querySelector(".public");
+    const private = document.querySelector(".private");
+    publico.classList.remove("selecionado");
+    private.classList.add("selecionado");
+    type = "private_message";
+}
+
+function selecionarPublico(){
+    const publico = document.querySelector(".public");
+    const private = document.querySelector(".private");
+    publico.classList.add("selecionado");
+    private.classList.remove("selecionado");
+    type = "message";
 }
